3630f9e92d363aa9c0e233f50571b9411f5bed13
Hamanpreet <haman42626@gmail.com>
2023-05-12 23:56:14 -0400

    Recursion for sum a to b

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 
-function sum(fromN, toN) {
+const sum = function(fromN, toN) {
   // Sum all the values from fromN up to toN
+  let sum1 = 0;
+  if ((fromN - toN) === 0) {
+    return sum1 += fromN;
+  }
+  
+  
+  
+  return fromN++ + sum(fromN, toN);
 }
+console.log(sum(2, 4));
 
 module.exports = sum;
